ef11e6e2a53f5de83b6cc56ce5c7962f19abedb0
daobin <shouwenlai@foxmail.com>
2021-04-25 23:59:20 +0800

    训练营2周作业

--- error-dao-demo.go
+++ error-dao-demo.go
@@ -6,7 +6,7 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-type User struct {
+type Test struct {
 	id int
 	name string
 	age int
@@ -15,39 +15,48 @@ type User struct {
 var db *sql.DB
 
 func initDB() (err error) {
-	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/user")
+	db, err := sql.Open("mysql", "xxx:xxx@tcp(127.0.0.1:3306)/kratos")
 	if err != nil {
-		panic(err)
+		// 直接返回错误给调用者
 		return
 	}
 
 	defer db.Close()
 
+	err = db.Ping()
+	if err != nil {
+		// 直接返回错误给调用者
+		return
+	}
+
 	return nil
 }
 
-func queryRow(id int) (User, error) {
-	var user User
-	sql := "select `id`, `name`, `age` from `user` where `id` = ?"
+func queryRow(id int) (Test, error) {
+	var test Test
+	sql := "select `id`, `name`, `age` from `test` where `id` = ?"
 	row := db.QueryRow(sql, id)
-	err := row.Scan(&user.id, &user.name, &user.age)
+	err := row.Scan(&test.id, &test.name, &test.age)
 
-	return user, err
+	// 当查询无结果错误时，直接返回给调用者判断。想像不到包装错误的使用场景，请老师解惑。
+	return test, err
 }
 
 func main()  {
 	err := initDB()
 	if err != nil {
-		fmt.Println("DB init failed, err = %v\n", err)
+		fmt.Printf("DB init failed, err >> %v\n", err)
 		return
 	}
 
-	user, err := queryRow(1)
-	if err != nil {
-		fmt.Println("DB query failed, err = %v\n", err)
-		return
-	}
+	// 查询数据居然被360识别为木马，请问老师，在 win 系统上开发如何解决
 
-	fmt.Println(user)
+	//test, err := queryRow(1)
+	//if err != nil {
+	//	fmt.Printf("DB query failed, err >> %v\n", err)
+	//	return
+	//}
+	//
+	//fmt.Println(test)
 }
 
