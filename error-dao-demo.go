56e130da5ec592a8ada6e8624951a335d9ee2d4a
laidaobin <shouwenlai@foxmail>
2021-04-25 18:46:26 +0800

    change error-dao-demo

--- error-dao-demo.go
+++ error-dao-demo.go
@@ -1,26 +1,35 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	xerrors "github.com/pkg/errors"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
 )
 
-var errMy = errors.New("my")
-
-func test0() error {
-	return xerrors.Wrapf(errMy, "test0 failed")
+type TestData struct {
+	Name string `db:"name"`
+	Age int `db:"age"`
 }
 
-func test1() error {
-	return test0()
-}
+var Db *sqlx.DB
 
-func test2() error {
-	return test1()
+func init() {
+	Db, err := sqlx.Connect("mysql", "root:root@tcp(127.0.0.21:3306)/test_data")
+	if err != nil {
+		fmt.Println("Connect Mysql Failed, ", err)
+		return
+	}
+
+	defer Db.Close()
 }
 
 func main()  {
-	err := test2()
-	fmt.Println("main: %+v\n", err)
+	var td []TestData
+	err := Db.Get(td, "select `name`, `age` from `test_data` where `age` = ?", 18)
+	if err != nil {
+		fmt.Println("Exec Failed: ", err)
+	}
+
+	fmt.Println(td)
 }
+
